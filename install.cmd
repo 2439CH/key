6f7214935fdfa8e302b62b956de1084854ac4f4d
Shop D I N O <donut2.9K@gmail.com>
2025-07-23 14:49:46 +0700

    Create install.cmd

---
+++ install.cmd
@@ -0,0 +1,12 @@
+python.exe -m pip install --upgrade pip
+python -m pip install -U pip
+pip install requests
+pip install bs4
+pip install webdriver_manager
+pip install selenium
+pip install pillow
+pip install pytesseract
+pip install distutils-pytest
+pip install setuptools
+pip install undetected-chromedriver
+pip install tqdm
